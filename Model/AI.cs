ba84a9cbe4f7709e61b54c1df4a97e096297b3c4
stan-his <niclas.stahl@ju.se>
2021-09-22 09:17:51 +0200

    Check-in of full BlazorConnect4 program

---
+++ Model/AI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BlazorConnect4.Model;
+
+namespace BlazorConnect4.AIModels
+{
+
+    public abstract class AI
+    {
+        // Funktion för att beskriva 
+        public abstract int SelectMove(Cell[,] grid);
+    }
+
+
+    public class RandomAI : AI
+    {
+        Random generator;
+
+        public RandomAI()
+        {
+            generator = new Random();
+        }
+
+        public override int SelectMove(Cell[,] grid)
+        {
+            return generator.Next(7);
+        }
+    }
+}
---
+++ Model/Model.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using BlazorConnect4.AIModels;
+
+namespace BlazorConnect4.Model
+{
+    public enum CellColor
+    {
+        Red,
+        Yellow,
+        Blank
+    }
+
+
+    public class Cell
+    {
+        public CellColor Color {get; set;}
+
+        public Cell(CellColor color)
+        {
+            Color = color;
+        }
+
+    }
+
+    public class GameBoard
+    {
+        public Cell[,] Grid { get; set; }
+
+        public GameBoard()
+        {
+            Grid = new Cell[7, 6];
+
+            //Populate the Board with blank pieces
+            for (int i = 0; i <= 6; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    Grid[i, j] = new Cell(CellColor.Blank);
+                }
+            }
+        }
+
+
+    }
+
+
+    public class GameEngine
+    {
+        public GameBoard Board { get; set; }
+        public CellColor Player { get; set;}
+        public bool active;
+        public String message;
+        private AI ai;
+
+
+        public GameEngine()
+        {
+            Reset("Human");
+        }
+
+
+
+        // Reset the game and creats the opponent.
+        // TODO change the code so new RL agents are created.
+        public void Reset(String playAgainst)
+        {
+            Board = new GameBoard();
+            Player = CellColor.Red;
+            active = true;
+            message = "Starting new game";
+
+            if (playAgainst == "Human")
+            {
+                ai = null;
+            }
+            else if (playAgainst == "Random")
+            {
+                ai = new RandomAI();
+            }
+            else if (playAgainst == "Q1")
+            {
+                ai = new RandomAI();
+            }
+            else if (playAgainst == "Q2")
+            {
+                ai = new RandomAI();
+            }
+            else if (playAgainst == "Q3")
+            {
+                ai = new RandomAI();
+            }
+
+        }
+
+
+
+
+        private bool IsValid(int col)
+        {
+            return Board.Grid[col, 0].Color == CellColor.Blank;
+        }
+
+
+        public bool IsDraw()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (Board.Grid[i,0].Color == CellColor.Blank)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public bool IsWin(int col, int row)
+        {
+            bool win = false;
+            int score = 0;
+            
+
+            // Check down
+            if (row < 3)
+            {
+                for (int i = row; i <= row + 3; i++)
+                {
+                    if (Board.Grid[col,i].Color == Player)
+                    {
+                        score++;
+                    }
+                }
+                win = score == 4;
+                score = 0;
+            }
+
+            // Check horizontal
+
+            int left = Math.Max(col - 3, 0);
+
+            for (int i = left; i <= col; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i+j <= 6 && Board.Grid[i+j,row].Color == Player)
+                    {
+                        score++;
+                    }
+                }
+                win = win || score == 4;
+                score = 0;
+            }
+
+            // Check left down diagonal
+
+            int colpos;
+            int rowpos;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    colpos = col - i + j;
+                    rowpos = row - i + j;
+                    if (0 <= colpos && colpos <= 6 &&
+                        0 <= rowpos && rowpos < 6 &&
+                        Board.Grid[colpos,rowpos].Color == Player)
+                    {
+                        score++;
+                    }
+                }
+
+                win = win || score == 4;
+                score = 0;
+            }
+
+            // Check left up diagonal
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    colpos = col - i + j;
+                    rowpos = row - i - j;
+                    if (0 <= colpos && colpos <= 6 &&
+                        0 <= rowpos && rowpos < 6 &&
+                        Board.Grid[colpos, rowpos].Color == Player)
+                    {
+                        score++;
+                    }
+                }
+
+                win = win || score == 4;
+                score = 0;
+            }
+
+            return win;
+        }
+
+
+
+
+        public bool Play(int col)
+        {
+            if (IsValid(col) && active){
+
+                for (int i = 5; i >= 0; i--)
+                {
+                    if (Board.Grid[col, i].Color == CellColor.Blank)
+                    {
+                        Board.Grid[col, i].Color = Player;
+
+                        if (IsWin(col,i))
+                        {
+                            message = Player.ToString() + " Wins";
+                            active = false;
+                            return true;
+                        }
+
+                        if (IsDraw())
+                        {
+                            message = "Draw";
+                            active = false;
+                            return true;
+                        }
+                        break;
+                    }
+                }
+                return PlayNext();
+            }
+
+            return false;
+        }
+
+
+
+
+        private bool PlayNext()
+        {
+
+            if (Player == CellColor.Red)
+            {
+                Player = CellColor.Yellow;
+            }
+            else
+            {
+                Player = CellColor.Red;
+            }
+
+            if (ai != null && Player == CellColor.Yellow)
+            {
+                int move = ai.SelectMove(Board.Grid);
+
+                while (! IsValid(move))
+                {
+                    move = ai.SelectMove(Board.Grid);
+                }
+
+                return Play(move);
+            }
+
+            return false;
+        }
+    }
+
+
+}
